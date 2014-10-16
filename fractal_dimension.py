8383a87d681c7f5a2294e2c9242edf08b00ea027
Howon Lee <howon@howonlee.com>
2014-10-15 21:37:35 -0700

    fractal dimension by box counting

---
+++ fractal_dimension.py
@@ -0,0 +1,17 @@
+import numpy as np
+import numpy.random as npr
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+import networkx as nx
+from nltk.corpus import brown
+import collections
+from operator import itemgetter
+from random import choice
+import random
+
+if __name__ == "__main__":
+    get the bigram matrix, create that sucker
+    normalize
+    bound it via like 0.005 or something
+    count that fractal dimension via boxes
+    probably can also count lacunarity via box counting
