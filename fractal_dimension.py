c6225e15b43d2fac78ede7a5db9f35ed5ec50388
Howon Lee <howon@howonlee.com>
2014-10-18 22:40:36 -0700

    fractal dimension is there

--- fractal_dimension.py
+++ fractal_dimension.py
@@ -6,8 +6,6 @@ import matplotlib.cm as cm
 import networkx as nx
 from nltk.corpus import brown
 import collections
-from operator import itemgetter
-from random import choice
 import random
 
 def word_mapping(words):
@@ -17,8 +15,13 @@ def word_mapping(words):
         if word in word_dict:
             pass #do nothing
         else:
-            word_dict[word] = curr_count
+            word_dict[word] = -1
             curr_count += 1
+    #shuffle the range, we should have no -1's afterwards
+    word_range = range(curr_count)
+    random.shuffle(word_range)
+    for key in word_dict:
+        word_dict[key] = word_range.pop()
     return word_dict
 
 def get_bigrams(ls):
@@ -30,18 +33,30 @@ def bigram_to_mat(bigram, word_map):
         mat[word_map[prevword], word_map[word]] += 1
     return mat
 
+def get_indiv_box_count(i, j, box_size, mat):
+    for k in xrange(0, box_size):
+        for l in xrange(0, box_size):
+            if mat.has_key((i + k, j + l)):
+                return 1
+    return 0
+
+def get_box_count(box_size, mat):
+    count = 0
+    for i in xrange(0, mat.shape[0], box_size):
+        for j in xrange(0, mat.shape[0], box_size):
+            count += get_indiv_box_count(i, j, box_size, mat)
+    return count
+
 if __name__ == "__main__":
     brown_words = brown.words()
     print len(brown_words)
     bigrams = get_bigrams(brown_words)
     word_dict = word_mapping(brown_words)
     mat = bigram_to_mat(bigrams, word_dict)
-    plt.spy(mat, markersize=0.3)
+    box_sizes = [256, 128, 64, 32, 16, 8, 4, 2, 1] #let's try to think about this sort of thing
+    box_counts = []
+    for box_size in box_sizes:
+        print "getting box size ", box_size
+        box_counts.append(get_box_count(box_size, mat))
+    plt.loglog(box_sizes, box_counts)
     plt.show()
-
-    #get the bigram matrix, create that sucker
-    #normalize
-    #bound it via like 0.005 or something
-    #count that fractal dimension via boxes
-    #probably can also count lacunarity via box counting
-    #and then use it for nice things
