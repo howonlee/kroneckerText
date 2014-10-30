259d5063e18b8e498de63d6278db36735e80eac7
Howon Lee <howon@howonlee.com>
2014-10-30 16:39:39 -0700

    there are so many things wrong with me

--- mcmc.py
+++ mcmc.py
@@ -5,31 +5,27 @@ import numpy as np
 import numpy.random as npr
 import matplotlib.pyplot as plt
 import operator
+import random
 
-def lnprob(x, prior, switch):
+def lnprob(x, graph, theta):
     """
     This is amenable to a trick: only compute twice in each case...
     """
     return -0.5 * np.sum(x ** 2)
 
-def run_mcmc(ndim, lnprob, p0, num_steps=1000):
-    curr_probs = np.array(p0)
-    curr_lnprob = lnprob(curr_probs)
-    for i in xrange(num_steps):
-        curr_q = npr.multivariate_normal(curr_probs, np.identity(5)) #covariance is just default stuff
-        new_lnprob = lnprob(curr_q)
-        diff = new_lnprob - curr_lnprob
-        if diff < 0:
-            diff = np.exp(diff) - npr.rand()
-        else:
-            curr_probs = curr_q
-            curr_lnprob = new_lnprob
-        yield curr_probs, curr_lnprob
+def swap_nodes(ls_sigma, j, k):
+    ls_sigma[k], ls_sigma[j] = ls_sigma[j], ls_sigma[k]
+    return tuple(ls_sigma)
 
 if __name__ == "__main__":
-    p0 = npr.random(5)
-    p = list(run_mcmc(5, lnprob, p0))
-    ps = map(operator.itemgetter(0), p)
-    ps = map(operator.itemgetter(0), ps)
-    plt.plot(range(1000), ps)
-    plt.show()
+    N = 10
+    sigma = tuple(range(N))
+    prev_sigma = tuple(range(N))
+    for i in xrange(10000)
+        j = random.randint(0, N-1)
+        k = random.randint(0, N-1)
+        sigma = swap_nodes(list(sigma), j, k)
+        u = random.random()
+        if u > (lnprob(sigma, graph, theta) / lnprob(prev_sigma, graph, theta)):
+            sigma = prev_sigma
+    return sigma
