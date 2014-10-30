1f8e0640a5b2714ee10e81b1b32be10d9eb1a552
Howon Lee <howon@howonlee.com>
2014-10-30 13:59:24 -0700

    we have another state picture

--- state_picture.py
+++ state_picture.py
@@ -1,5 +1,6 @@
 import cPickle
 import scipy.sparse as sci_sp
+import numpy as np
 import itertools
 import random
 import matplotlib.pyplot as plt
@@ -8,12 +9,19 @@ def plot_sparse(mat):
     plt.spy(mat, markersize=0.1)
     plt.show()
 
+def plot_powerlaw(mat, max_gut_state):
+    degrees = mat.sum(axis=1)
+    degrees = list(np.sort(np.ravel(degrees)))
+    degrees.reverse()
+    print len(degrees)
+    plt.loglog(range(max_gut_state+1),degrees)
+    plt.show()
+
 if __name__ == "__main__":
     gutenberg_states = []
     with open("brown_states.txt", "r") as states_file:
         gutenberg_states = cPickle.load(states_file)
     gutenberg_states = gutenberg_states[:400000]
-    random.shuffle(gutenberg_states) ##experiment
     max_gut_state = max(gutenberg_states)
     print max_gut_state
     sp_statemat = sci_sp.dok_matrix((max_gut_state+1, max_gut_state+1))
@@ -25,4 +33,6 @@ if __name__ == "__main__":
         sp_statemat[x,y] += 1
         if state % 10000 == 0:
             print "state: ", state
-    plot_sparse(sp_statemat)
+    sp_statemat = sp_statemat.tocsr()
+    #plot_sparse(sp_statemat)
+    plot_powerlaw(sp_statemat, max_gut_state)
