2e0a5e676235e42ce6063f73bdd5830098346783
Howon Lee <howon@howonlee.com>
2014-10-30 16:06:55 -0700

    9 will do, mostly, but that's for our corpus

--- state_picture.py
+++ state_picture.py
@@ -3,6 +3,7 @@ import scipy.sparse as sci_sp
 import numpy as np
 import itertools
 import random
+import collections
 import math
 import matplotlib.pyplot as plt
 
@@ -20,6 +21,7 @@ def plot_lognormal(mat):
 
 def plot_powerlaw(mat):
     degrees = mat.sum(axis=1)
+    degrees = degrees.sum(axis=1)
     degrees = list(np.sort(np.ravel(degrees)))
     degrees.reverse()
     plt.loglog(range(len(degrees)),degrees)
@@ -28,22 +30,31 @@ def plot_powerlaw(mat):
 def thresh_mat(mat):
     return mat.multiply(mat>3)
 
+def plot_degrees_powerlaw(degrees):
+    degrees = sorted(degrees)
+    degrees.reverse()
+    plt.loglog(range(len(degrees)),degrees)
+    plt.show()
+
 if __name__ == "__main__":
     gutenberg_states = []
     with open("brown_states.txt", "r") as states_file:
         gutenberg_states = cPickle.load(states_file)
-    gutenberg_states = gutenberg_states[:400000]
+    #gutenberg_states = gutenberg_states[:7000]
     max_gut_state = max(gutenberg_states)
     print max_gut_state
-    sp_statemat = sci_sp.dok_matrix((max_gut_state+1, max_gut_state+1))
-    uni, bi = itertools.tee(gutenberg_states)
+    sp_statemat = collections.Counter()
+    uni, bi, tri, quad= itertools.tee(gutenberg_states, 4)
     bi.next()
+    tri.next(); tri.next()
+    quad.next(); quad.next(); quad.next()
     state = 0
-    for x, y in itertools.izip(uni, bi):
+    for x, y, z, xx in itertools.izip(uni, bi, tri, quad):
         state += 1
-        sp_statemat[x,y] += 1
+        sp_statemat[(x,y,z,xx)] += 1
         if state % 10000 == 0:
             print "state: ", state
     #plot_sparse(sp_statemat)
     #plot_powerlaw(sp_statemat)
-    plot_lognormal(sp_statemat)
+    #plot_lognormal(sp_statemat)
+    plot_degrees_powerlaw([y for x,y in sp_statemat.most_common()])
