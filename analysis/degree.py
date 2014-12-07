a882e0b92d6efffb2883c78ce50867bbe327351f
Howon Lee <howon@howonlee.com>
2014-12-06 18:54:46 -0800

    rename to singular values

--- analysis/degree.py
+++ analysis/degree.py
@@ -7,12 +7,14 @@ from scipy.stats import ks_2samp
 import numpy as np
 
 net = nx.DiGraph()
-words = brown.words()[:500000]
+words = brown.words()
 net.add_nodes_from(words)
 for word, prevword in zip(words, words[1:]):
     net.add_edge(word, prevword)
 degree_sequence=sorted(nx.degree(net).values(),reverse=True)
 plt.loglog(degree_sequence, 'b-')
-plt.title("degree loglog histogram")
-plt.show()
+plt.title("Degree Counts on Brown Corpus")
+plt.xlabel("Degree")
+plt.ylabel("Count")
+plt.savefig("degree_plot")
 
