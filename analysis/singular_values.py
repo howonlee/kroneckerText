52778147d11eec67850df1b0fda197e7d514bf9a
Howon Lee <howon@howonlee.com>
2014-12-06 19:03:29 -0800

    diameter was a bit bullcrap

--- analysis/singular_values.py
+++ analysis/singular_values.py
@@ -8,15 +8,16 @@ import numpy as np
 import numpy.linalg as npl
 
 net = nx.DiGraph()
-words = brown.words()[:1000]
+words = brown.words()[:10000]
 net.add_nodes_from(words)
 for word, prevword in zip(words, words[1:]):
     net.add_edge(word, prevword)
 adj = nx.to_numpy_matrix(net)
 U, S, V = npl.svd(adj)
 eigs = S ** 2 / np.cumsum(S)[-1]
+eigs = eigs[eigs>0.0005]
 
-plt.loglog(eigs[:200], 'b-')
+plt.loglog(eigs, 'b-')
 plt.xlabel("rank")
-plt.ylabel("network value")
-plt.savefig("eigenvector_loglog")
+plt.ylabel("singular value")
+plt.savefig("singval_loglog")
