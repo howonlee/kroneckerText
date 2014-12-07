8842722536c06e37f66a75e7786f64829fe5909e
Howon Lee <howon@howonlee.com>
2014-12-06 18:38:31 -0800

    remove a lot of features we can't reproduce

--- pos/tagger.py
+++ pos/tagger.py
@@ -128,16 +128,6 @@ class PerceptronTagger(BaseTagger):
         add('i suffix', word[-3:])
         add('i pref1', word[0])
         add('i-1 tag', prev)
-        add('i-2 tag', prev2)
-        add('i tag+i-2 tag', prev, prev2)
-        add('i word', context[i])
-        add('i-1 tag+i word', prev, context[i])
-        add('i-1 word', context[i-1])
-        add('i-1 suffix', context[i-1][-3:])
-        add('i-2 word', context[i-2])
-        add('i+1 word', context[i+1])
-        add('i+1 suffix', context[i+1][-3:])
-        add('i+2 word', context[i+2])
         return features
 
     def _make_tagdict(self, sentences):
