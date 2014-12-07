107e532523d565efa1eeabb488e00c558c78df4c
Howon Lee <howon@howonlee.com>
2014-12-06 21:41:17 -0800

    analysis goes over the top

--- analysis/stack_overflow_process.py
+++
@@ -1,7 +0,0 @@
-with open("stack_overflow.txt", "r") as stack_file:
-    for line in stack_file:
-        first, second = tuple(line.split())
-        if first == "__none__" or second == "__none__": continue
-        first, second= int(first), int(second)
-        if first < 10000 and second < 10000:
-            print first, second
