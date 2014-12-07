8cee98fa9dc081a60e220dd8d9bc380cefee1deb
Howon Lee <howon@howonlee.com>
2014-12-07 12:04:16 -0800

    we are closer to having confusion matrix measures

--- pos/test.py
+++ pos/test.py
@@ -1,6 +1,24 @@
 from tagger import PerceptronTagger
+from nltk.corpus import brown
+
+def traintest_split(sentence_list):
+    trains = []
+    tests = []
+    for idx, val in enumerate(sentence_list):
+        if some hash: ##############
+            trains.append(val)
+        else:
+            tests.append(val)
+    return trains, tests
+
+def tag_strip(sentence_list):
+    pass
 
 if __name__ == "__main__":
     text = "Simple is better than complex. Complex is better than complicated."
+    sentences = brown.tagged_sents()
+    train, test = traintest_split(brown.tagged_sents())
     tagger= PerceptronTagger(load=False)
-    tagger.train(sentences, nr_iter)
+    tagger.train(train, nr_iter)
+    stripped_tests = tag_strip(tests)
+    tagger.test(tests, should be a validation set somewhere)
