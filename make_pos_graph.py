ed37fa41d192925e9dd08eda64a67701eb230b9c
Howon Lee <howon@howonlee.com>
2014-12-07 21:22:15 -0800

    what a pickle!

--- make_pos_graph.py
+++ make_pos_graph.py
@@ -1,6 +1,7 @@
 import collections
 import operator
 import math
+import cPickle
 from nltk.corpus import brown
 import networkx as nx
 import matplotlib.pyplot as plt
@@ -16,6 +17,8 @@ for tag in tags:
         tags_dict[tag] = curr_idx
         curr_idx += 1
 net.add_nodes_from(tags_dict.values())
-for word, prevword in zip(tags, tags[1:]):
-    net.add_edge(tags_dict[word], tags_dict[prevword])
-nx.write_edgelist(net, "brown_pos_graph.txt", data=False)
+#for word, prevword in zip(tags, tags[1:]):
+#    net.add_edge(tags_dict[word], tags_dict[prevword])
+#nx.write_edgelist(net, "brown_pos_graph.txt", data=False)
+with open("pos_dict.pickle", "wb") as pickle_file:
+    cPickle.dump(tags_dict, pickle_file)
