c51ad9d100a5625b003307b71cd6287018351f01
Howon Lee <howon@howonlee.com>
2014-12-08 11:31:06 -0800

    play with the thought of it

--- pos/make_pos_graph_lang.py
+++ pos/make_pos_graph_lang.py
@@ -5,4 +5,5 @@ import matplotlib.pyplot as plt
 if __name__ == "__main__":
     net = nx.read_edgelist("../gen_pos_graph.txt")
     adj = nx.to_numpy_matrix(net)
-    #make the labels
+    #assign the labels... there's some previous code for this
+    #then, take it as a Markov chain. How?
