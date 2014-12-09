166a5990ddee504a15f9835b43d7a08b7cafef80
Howon Lee <howon@howonlee.com>
2014-12-09 14:58:46 -0800

    no need for the 4000

--- 4000/gen_text.py
+++
@@ -1,7 +0,0 @@
-from nltk.corpus import brown
-import cPickle
-
-if __name__ == "__main__":
-    ls = [x for x in brown.words()]
-    with open("brown.txt", "w") as brownfile:
-        cPickle.dump(ls, brownfile)
