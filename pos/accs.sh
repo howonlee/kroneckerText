2066b7d22c8ed7c0585c4ccbb56ab9c5f7c8e4a8
Howon Lee <howon@howonlee.com>
2014-12-09 13:21:09 -0800

    convenience script

---
+++ pos/accs.sh
@@ -0,0 +1,5 @@
+python get_acc.py gen_res_conf_mat.npy
+python get_acc.py kron_conf_mat.npy
+python get_acc.py bigram_conf_mat.npy
+python get_acc.py kron2_conf_mat.npy
+python get_acc.py kron_deg_conf_mat.npy
