966a9d124b10b7ff01736e0d91fadf001d63503a
Howon Lee <howon@howonlee.com>
2014-12-08 23:48:06 -0800

    f1

---
+++ pos/f1s.sh
@@ -0,0 +1,5 @@
+python get_f1.py gen_res_conf_mat.npy
+python get_f1.py kron_conf_mat.npy
+python get_f1.py bigram_conf_mat.npy
+python get_f1.py kron2_conf_mat.npy
+python get_f1.py kron_deg_conf_mat.npy
