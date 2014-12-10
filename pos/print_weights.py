7d155328d26a5a0869dd31f287b24cad1b21d6b0
Howon Lee <howon@howonlee.com>
2014-12-09 19:51:15 -0800

    just citations now

--- pos/print_weights.py
+++ pos/print_weights.py
@@ -3,5 +3,5 @@ from tagger import PerceptronTagger
 if __name__ == "__main__":
     tagger= PerceptronTagger(load="kron_deg")
     for key, val in tagger.model.weights.items():
-        if "degree" in key and len(val) > 50:
+        if "degree" in key and len(val) > 150:
             print key, len(val)
