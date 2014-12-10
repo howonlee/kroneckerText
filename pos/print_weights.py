36ebbbecc831a3d93e4bea3fd72efac298c948b0
Howon Lee <howon@howonlee.com>
2014-12-09 17:24:59 -0800

    overfitting, eh?

--- pos/print_weights.py
+++ pos/print_weights.py
@@ -3,5 +3,5 @@ from tagger import PerceptronTagger
 if __name__ == "__main__":
     tagger= PerceptronTagger(load="kron_deg")
     for key, val in tagger.model.weights.items():
-        if u"degree" in key and len(val) < 10:
-            print key, val
+        if "degree" in key and len(val) > 50:
+            print key, len(val)
